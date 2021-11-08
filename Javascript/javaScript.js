32ef7f69f9d0f0fb7e247e02746e2a1254b01cc1
QuillanW <56843492+QuillanW@users.noreply.github.com>
2021-11-08 16:32:21 +0100

    dfgn

--- Javascript/javaScript.js
+++ Javascript/javaScript.js
@@ -2,9 +2,13 @@
     var scroll = document.scrollingElement.scrollTop
     console.log(scroll)
     if (scroll == 0) {
-        $("#contentcollapsible").fadeIn(150);
+        document.getElementById("contentcollapsible").style.position = "";
+        document.getElementById("contentcollapsible").style.marginTop = "";
+        $("#contentcollapsible").fadeIn(300);
     } else {
-        $("#contentcollapsible").fadeOut(150);
+        document.getElementById("contentcollapsible").style.position = "fixed";
+        document.getElementById("contentcollapsible").style.marginTop = "-10%";
+        $("#contentcollapsible").fadeOut(300);
     }
     setTimeout(arguments.callee, 50);
 })();
